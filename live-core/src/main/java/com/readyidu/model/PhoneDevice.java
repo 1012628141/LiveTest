71c1c630900de4a7f0ccaa95eb29a9f9797f0901
yupengfei <314574803@qq.com>
2017-12-25 14:15:06 +0800

    model以及mapper

---
+++ live-core/src/main/java/com/readyidu/model/PhoneDevice.java
@@ -0,0 +1,52 @@
+package com.readyidu.model;
+
+/**
+ * Created by ypf on 2017/12/25.
+ */
+public class PhoneDevice {
+    private Integer id;
+    private String phoneName;
+    private String phoneId;
+    private Integer tvDeviceId;
+    private Integer userId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPhoneName() {
+        return phoneName;
+    }
+
+    public void setPhoneName(String phoneName) {
+        this.phoneName = phoneName;
+    }
+
+    public String getPhoneId() {
+        return phoneId;
+    }
+
+    public void setPhoneId(String phoneId) {
+        this.phoneId = phoneId;
+    }
+
+    public Integer getTvDeviceId() {
+        return tvDeviceId;
+    }
+
+    public void setTvDeviceId(Integer tvDeviceId) {
+        this.tvDeviceId = tvDeviceId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+}
